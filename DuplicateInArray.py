25b74d4b29887e1e573523234b60f4788877f0a1
purvasheth <31120070+purvasheth@users.noreply.github.com>
2020-06-16 16:20:08 +0530

    Update DuplicateInArray.py

--- DuplicateInArray.py
+++ DuplicateInArray.py
@@ -22,7 +22,7 @@ class Solution:
 #             fast = nums[fast]
 #         return fast
         
-    #Last most efficient just O(n)
+    #Last most efficient just O(n) negate numbers
     
         for n in nums:
             if(nums[abs(n)]<0):
@@ -30,3 +30,29 @@ class Solution:
             nums[abs(n)] = -nums[abs(n)]
         
         return None
+
+def stringToIntegerList(input):
+    return json.loads(input)
+
+def main():
+    import sys
+    import io
+    def readlines():
+        for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            nums = stringToIntegerList(line);
+            
+            ret = Solution().findDuplicate(nums)
+
+            out = str(ret);
+            print(out)
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
