bbdafbaaf6da7843e174f55d1c9cf1888ac96e1f
purvasheth <31120070+purvasheth@users.noreply.github.com>
2020-07-02 18:19:51 +0530

    Create 017.TwoSum.py

---
+++ 017.TwoSum.py
@@ -0,0 +1,50 @@
+# Question - https://leetcode.com/problems/two-sum/
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #hashmap
+        hashmap = {}
+        for i in range(len(nums)):
+            if nums[i] not in hashmap.keys():
+                hashmap[target-nums[i]] = i
+            else:
+                return [hashmap[nums[i]],i]
+        
+         #brute force
+
+        # for i in range(len(nums)):
+        #         for j in range(i+1,len(nums)):
+        #             if nums[i]+nums[j]==target:
+        #                 return [i,j]
+
+def stringToIntegerList(input):
+    return json.loads(input)
+
+def integerListToString(nums, len_of_list=None):
+    if not len_of_list:
+        len_of_list = len(nums)
+    return json.dumps(nums[:len_of_list])
+
+def main():
+    import sys
+    import io
+    def readlines():
+        for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            nums = stringToIntegerList(line);
+            line = next(lines)
+            target = int(line);
+            
+            ret = Solution().twoSum(nums, target)
+
+            out = integerListToString(ret);
+            print(out)
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
